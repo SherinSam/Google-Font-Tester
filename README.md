acb363bd5a76cd15423ec0cde4e5fbc260349e9e
Sherin <samuel.sherin2@gmail.com>
2018-12-07 13:40:05 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+
+## Google Fonts Tester
+
+#### Purpose:
+To test the various google fonts with different colors and font sizes. A faster alternative to checking the same in Google fonts website.
+
+#### Instructions:
+1. Slider lets you select the font size of the displayed text.
+2. Select the required Google font from the dropdown.
+3. Move mouse over the text to see it getting displayed in different colors. Color name and hex code will be displayed on the top.
+
+
+### Google Fonts API Link
+https://developers.google.com/fonts/docs/developer_api
+
+
+### Links that helped
+1. https://stackoverflow.com/questions/22445760/how-to-change-css-href-using-javascript
+2. https://stackoverflow.com/questions/524696/how-to-create-a-style-tag-with-javascript
+3. http://chir.ag/projects/ntc/ntc.js
+
+### Future Reference
+https://github.com/typekit/webfontloader
+
+### Future Enhancements
+1. Incorporate styles such as Italics  to each font.
---
+++ index.js
@@ -0,0 +1,63 @@
+const input  = document.querySelector('#size')
+const text   = document.querySelector('.text')   
+const select = document.querySelector('.fontselect') 
+
+
+
+function fontsizeUpdate(){         
+    const size = document.querySelector('.fontsize')     
+    const suffix = this.dataset.sizing || '';
+    //document.documentElement selects <html>
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)  
+    size.textContent = this.value + suffix
+}
+
+
+function colorUpdate(){   
+    const color  = document.querySelector('.colorname')    
+    //generate and apply a random color on text
+    const randColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+    text.style.setProperty('color', randColor)                          
+    //display the color name and hex code. refer index.html for info on ntc
+    color.textContent = `${ntc.name(randColor)[1]} ${randColor}`  
+    color.style.setProperty('color', randColor)            
+}
+
+
+function fontfamilyUpdate(){
+    const fontlink = document.querySelector('#fontlink')  
+
+    //generate font url. eg: url: 'https://fonts.googleapis.com/css?family=Anonymous+Pro:italic&subset=greek'
+    const apiUrl = [];
+    apiUrl.push('https://fonts.googleapis.com/css?family=');
+    apiUrl.push(this.value.replace(/ /g, '+'));
+    const url = apiUrl.join('');
+       
+    //replaces href='#' with the href='url' in <head>
+    fontlink.setAttribute('href', `${url}`) 
+    //changes font family of the text           
+    text.style.setProperty('font-family', this.value)  
+}
+
+
+//Populates dropdown menu with Google font family names from the response JSON
+const fontOptions = (fonts) => {
+    fonts.forEach(function(font){
+        const option = document.createElement('option')
+        option.text = font.family
+        select.add(option)
+    })
+}
+
+const fetchInfo = () => { 
+    const url ='https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyB0EB5dJQ-v_VkdWVNipodZag2i0-FhTsc';
+    fetch(url)
+        .then(resp => resp.json())
+        .then(json => fontOptions(json.items))
+        .catch(error => console.log(error));
+}
+
+fetchInfo();                                           //Fetch Google fonts in JSON format
+input.addEventListener('change', fontsizeUpdate)       //update fontsize when slider is moved    
+text.addEventListener('mousemove', colorUpdate)        //show text in a random color on mouse move. display colorcode
+select.addEventListener('change', fontfamilyUpdate)    //update font family as per the selected option
